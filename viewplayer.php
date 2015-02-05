1ed4b6d7986586807b8fca8f8e634631ab58a13c
Jonas Friedmann <j@frd.mn>
2015-02-05 02:25:21 +0100

    Fix display of actor name for past warnings
    Resolves #11 partially

--- viewplayer.php
+++ viewplayer.php
@@ -37,7 +37,7 @@ else {
 	$currentBans = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id FROM ".$server['playerBansTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
 	$pastBans = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id, HEX(pastActor_id) as pastActor_id FROM ".$server['playerBanRecordsTable']." WHERE player_id = UNHEX('".$UUID."') AND pastCreated <> 0", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
 	$currentMutes = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id FROM ".$server['playerMutesTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
-	$pastMutes = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id FROM ".$server['playerMuteRecordsTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
+	$pastMutes = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id, HEX(pastActor_id) as pastActor_id FROM ".$server['playerMuteRecordsTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
 	$pastKicks = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id FROM ".$server['playerKicksTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
 	$pastWarnings = cache("SELECT *, HEX(player_id) as player_id, HEX(actor_id) as actor_id FROM ".$server['playerWarningsTable']." WHERE player_id = UNHEX('".$UUID."')", $settings['cache_viewplayer'], $_GET['server'].'/players', $server);
 
