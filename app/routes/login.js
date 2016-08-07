4d57ee28523a9e9156c59725fca83480cde1275e
James Mortemore <jamesmortemore@gmail.com>
2016-08-07 17:42:39 +0100

    Prevent logged in users viewing the login form

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,7 @@
 import Ember from 'ember'
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin'
 
-export default Ember.Route.extend(
+export default Ember.Route.extend(UnauthenticatedRouteMixin,
   { model() {
       return this.store.findAll('server')
     }
