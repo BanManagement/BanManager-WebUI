bc17e2f9af73da98421e9c5f20af361eb3fef210
Jonas Friedmann <j@frd.mn>
2016-09-13 11:10:43 +0200

    Remove license headers

--- footer.php
+++ footer.php
@@ -1,14 +1,3 @@
-<?php
-/*
- *  BanManagement © 2015, a web interface for the Bukkit plugin BanManager
- *  by James Mortemore of http://www.frostcast.net
- *  is licenced under a Creative Commons
- *  Attribution-NonCommercial-ShareAlike 2.0 UK: England & Wales.
- *  Permissions beyond the scope of this licence
- *  may be available at http://creativecommons.org/licenses/by-nc-sa/2.0/uk/.
- *  Additional licence terms at https://raw.githubusercontent.com/BanManagement/BanManager-WebUI/master/LICENSE
- */
-?>
 			<hr>
 			<footer>
 				<p class="pull-left"><?php echo $settings['footer']; ?><?php if(isset($settings['admin_link']) && $settings['admin_link']){echo ' &mdash; <a href="index.php?action=admin" target="_blank" style="color:inherit;"><span class="glyphicon glyphicon-dashboard"></span></a>';} echo ' &mdash; <a href="https://github.com/BanManagement/BanManager-WebUI" target="_blank">BanManager WebUI</a> version '.returnVersion(); ?></p>
