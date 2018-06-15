d2dd61d7d1d6db7f8723e74d50b6566dfaa907f0
James Mortemore <jamesmortemore@gmail.com>
2018-06-15 22:02:14 +0100

    Refactored player page to use PageLayout component

---
+++ components/PageLayout.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Container,
+  Grid,
+  Responsive
+} from 'semantic-ui-react'
+
+// @TODO Component needs optimising, try and avoid numerous loops
+function calculateRowCount(components) {
+  return components.reduce(function (x, y) {
+    return (x.y > y.y) ? x.y : y.y
+  })
+}
+
+function createRows(rowCount, components) {
+  const rows = []
+
+  for (let i = 0; i <= rowCount; i++) {
+    rows[i] = []
+  }
+
+  components.forEach(deviceComponent => {
+    rows[deviceComponent.y].push(deviceComponent)
+  })
+
+  return rows
+}
+
+function createComponents(rows, availableComponents, props) {
+  return rows.map((row, i) => {
+    const components = row.sort((a, b) => a.x - b.x).map((deviceComponent, index) => {
+      const Component = availableComponents[deviceComponent.component]
+      const rendered =
+        <Grid.Column
+          width={deviceComponent.w}
+          color={deviceComponent.colour}
+          key={index}
+          textAlign={deviceComponent.textAlign}
+        >
+          <Container><Component {...props} /></Container>
+        </Grid.Column>
+
+      return rendered
+    })
+
+    return (
+      <Grid.Row key={i}>
+        {components}
+      </Grid.Row>
+    )
+  })
+}
+
+export default class PageLayout extends React.Component {
+  static propTypes =
+    { availableComponents: PropTypes.object.isRequired
+    , pageLayout: PropTypes.object.isRequired
+    }
+
+  render() {
+    const { availableComponents, pageLayout } = this.props
+    const devices = Object.keys(pageLayout.devices).filter(name => name !== '__typename')
+    const rowCounts = Object.assign({}, ...devices.map(device => {
+      return { [device]: calculateRowCount(pageLayout.devices[device]) }
+    }))
+    const rows = Object.assign({}, ...devices.map(device => {
+      return { [device]: createRows(rowCounts[device], pageLayout.devices[device]) }
+    }))
+    const deviceComponents = Object.assign({}, ...devices.map(device => {
+      return { [device]: createComponents(rows[device], availableComponents, this.props) }
+    }))
+
+    const components = <React.Fragment>
+      <Responsive {...Responsive.onlyMobile}>
+        <Grid style={{ marginTop: '1em' }}>{deviceComponents.mobile}</Grid>
+      </Responsive>
+      <Responsive {...Responsive.onlyTablet}>
+        <Grid style={{ marginTop: '1em' }}>{deviceComponents.tablet}</Grid>
+      </Responsive>
+      <Responsive {...Responsive.onlyComputer}>
+        <Grid >{deviceComponents.desktop}</Grid>
+      </Responsive>
+    </React.Fragment>
+
+    return components
+  }
+}
--- pages/player.js
+++ pages/player.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import withData from 'lib/withData'
 import DefaultLayout from 'components/DefaultLayout'
-import {
-  Button,
-  Container,
-  Grid,
-  Segment,
-  Responsive
-} from 'semantic-ui-react'
+import { Button, Container } from 'semantic-ui-react'
 import { Router } from 'routes'
+import PropTypes from 'prop-types'
 import PlayerHeader from 'components/PlayerHeader'
 import PlayerPunishmentList from 'components/PlayerPunishmentList'
 import PlayerPunishmentsQuery from 'components/queries/PlayerPunishmentsQuery'
@@ -16,16 +11,18 @@ import PlayerHistoryList from 'components/PlayerHistoryList'
 import PlayerIpList from 'components/PlayerIpList'
 import PlayerAlts from 'components/PlayerAlts'
 import PageLayoutQuery from 'components/queries/PageLayoutQuery'
+import PageLayout from 'components/PageLayout'
 
 const availableComponents = {
-  PlayerHeader,
-  PlayerPunishmentList,
-  PlayerIpList,
-  PlayerHistoryList,
-  PlayerAlts
+  PlayerHeader, PlayerPunishmentList, PlayerIpList, PlayerHistoryList, PlayerAlts
 }
 
 export class PlayerPage extends React.Component {
+  static propTypes =
+    { player: PropTypes.object.isRequired
+    , pathname: PropTypes.string.isRequired
+    }
+
   static async getInitialProps({ query, pathname }) {
     return {
       player: { id: query.id }, pathname
@@ -53,57 +50,13 @@ export class PlayerPage extends React.Component {
                 if (server.acl.warnings.create) canCreateWarning = true
               })
 
-              const rowCount = pageLayout.devices.desktop.reduce(function (x, y) {
-                return (x.y > y.y) ? x.y : y.y
-              })
-              const rows = []
-
-              for (let i = 0; i <= rowCount; i++) {
-                rows[i] = []
-              }
-
-              pageLayout.devices.desktop.forEach(deviceComponent => {
-                rows[deviceComponent.y].push(deviceComponent)
-              })
-
-              const desktopComponents = rows.map((row, i) => {
-                const components = row.sort((a, b) => a.x - b.x).map((deviceComponent, index) => {
-                  const Component = availableComponents[deviceComponent.component]
-                  const rendered =
-                    <Grid.Column
-                      width={deviceComponent.w}
-                      color={deviceComponent.colour}
-                      key={index}
-                      textAlign={deviceComponent.textAlign}
-                    >
-                      <Container><Component player={player} /></Container>
-                    </Grid.Column>
-
-                  return rendered
-                })
-
-                return (
-                  <Grid.Row key={i}>
-                    {components}
-                  </Grid.Row>
-                )
-              })
-
-              const components = <React.Fragment>
-                <Responsive {...Responsive.onlyMobile}>
-                  <Grid style={{ marginTop: '1em' }}>{desktopComponents}</Grid>
-                </Responsive>
-                <Responsive {...Responsive.onlyTablet}>
-                  <Grid>{desktopComponents}</Grid>
-                </Responsive>
-                <Responsive {...Responsive.onlyComputer}>
-                  <Grid>{desktopComponents}</Grid>
-                </Responsive>
-              </React.Fragment>
-
               return (
                 <DefaultLayout title={player.name}>
-                  {components}
+                  <PageLayout
+                    availableComponents={availableComponents}
+                    pageLayout={pageLayout}
+                    player={player}
+                  />
                   <Container style={{ marginTop: '1em' }}>
                     <Button.Group size='large' widths='4'>
                     { canCreateBan &&
@@ -113,10 +66,20 @@ export class PlayerPage extends React.Component {
                       <Button circular icon='mute' color='green' onClick={this.clickAddHandler('add-player-mute')} />
                     }
                     { canCreateNote &&
-                      <Button circular icon='sticky note outline' color='green' onClick={this.clickAddHandler('add-player-note')} />
+                      <Button
+                        circular
+                        icon='sticky note outline'
+                        color='green'
+                        onClick={this.clickAddHandler('add-player-note')}
+                      />
                     }
                     { canCreateWarning &&
-                      <Button circular icon='warning' color='green' onClick={this.clickAddHandler('add-player-warning')} />
+                      <Button
+                        circular
+                        icon='warning'
+                        color='green'
+                        onClick={this.clickAddHandler('add-player-warning')}
+                      />
                     }
                     </Button.Group>
                   </Container>
