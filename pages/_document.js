39c9e90bbd6e4627cca8376f7b8ed4355efe7ff8
James Mortemore <jamesmortemore@gmail.com>
2018-11-30 21:31:52 +0000

    chore(accessibility): added lang to <html>

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang='en'>
         <Head>
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
