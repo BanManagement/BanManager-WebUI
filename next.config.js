6d6f10f077738f37cceb55a77d08df31bcd38a2b
James Mortemore <james.mortemore@yld.io>
2018-12-13 16:36:11 +0000

    chore: added webpack analyser

--- next.config.js
+++ next.config.js
@@ -1,19 +1,37 @@
+const withPlugins = require('next-compose-plugins')
 const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS({
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+const nextConfig = {
   webpack(config) {
     config.module.rules.push(
-    { test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/
-    , use:
-        { loader: 'file-loader'
-        , options:
-          { publicPath: '/_next/static/'
-          , outputPath: 'static/'
+      { test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/
+      , use:
+          { loader: 'file-loader'
+          , options:
+            { publicPath: '/_next/static/'
+            , outputPath: 'static/'
+            }
           }
-        }
-      }
-    )
+      })
 
     return config
   }
-})
+}
+
+module.exports = withPlugins([ withCSS,
+  [ withBundleAnalyzer,
+    { analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/server.html'
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html'
+        }
+      }
+    }
+  ]
+], nextConfig)
