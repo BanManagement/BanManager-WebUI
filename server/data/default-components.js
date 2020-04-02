a0a04ca6c2509915cae9fc33ffc629c54027dd0a
James Mortemore <jamesmortemore@gmail.com>
2020-04-02 13:31:35 +0100

    fix: RecentServerPunishments is reusable

--- server/data/default-components.js
+++ server/data/default-components.js
@@ -1,4 +1,5 @@
 module.exports = [
   { component: 'HTML', meta: { html: '' } },
-  { component: 'ServerNameHeader', meta: { serverId: null, as: 'h2' } }
+  { component: 'ServerNameHeader', meta: { serverId: null, as: 'h2' } },
+  { component: 'RecentServerPunishments', meta: { serverId: null, type: 'bans' } }
 ].map(c => ({ w: 16, x: 0, y: 0, ...c }))
