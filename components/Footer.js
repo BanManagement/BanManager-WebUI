78649d12a627d0b1a70d00d47196b02523bfe36b
James Mortemore <jamesmortemore@gmail.com>
2020-09-20 12:55:58 +0100

    chore: clean up footer slightly

--- components/Footer.js
+++ components/Footer.js
@@ -14,8 +14,8 @@ export default function Footer ({ isMobileFromSSR }) {
               <List link inverted>
                 <List.Item as='a' href='https://dev.bukkit.org/projects/ban-management'>Bukkit Plugin</List.Item>
                 <List.Item as='a' href='https://ore.spongepowered.org/confuser/BanManager'>Sponge Plugin</List.Item>
-                <List.Item as='a' href='https://github.com/BanManagement/banmanagement.com'>Source</List.Item>
-                <List.Item as='a' href='https://github.com/BanManagement/banmanagement.com/issues'>Report Issues</List.Item>
+                {/* <List.Item as='a' href='https://github.com/BanManagement/banmanagement.com'>Source</List.Item> */}
+                {/* <List.Item as='a' href='https://github.com/BanManagement/banmanagement.com/issues'>Report Issues</List.Item> */}
               </List>
             </Grid.Column>
             {/* <Grid.Column width={3}>
@@ -31,9 +31,9 @@ export default function Footer ({ isMobileFromSSR }) {
               <Header as='h4' inverted>
                 BanManagement
               </Header>
-              <p>
-                James Mortemore &copy; 2012 - {yearNow}
-              </p>
+              {/* <p>
+                &copy; 2012 - {yearNow}
+              </p> */}
             </Grid.Column>
           </Grid.Row>
         </Grid>
