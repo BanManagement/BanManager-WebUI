50f69c39f4705a6903121c306eeaa54572dabd9f
James Mortemore <jamesmortemore@gmail.com>
2021-08-07 14:38:10 +0100

    try again

--- server/test/createServer.mutation.test.js
+++ server/test/createServer.mutation.test.js
@@ -192,7 +192,7 @@ describe('Mutation create server', () => {
     await pool('bm_players').insert(player)
 
     // Create temp user
-    await pool.raw('DROP USER foobar@localhost;')
+    await pool.raw('DROP USER \'foobar\'@\'localhost\';')
     await pool.raw('FLUSH PRIVILEGES;')
     await pool.raw('CREATE USER \'foobar\'@\'localhost\' IDENTIFIED BY \'password\';')
     await pool.raw('GRANT ALL ON *.* TO \'foobar\'@\'localhost\';')
