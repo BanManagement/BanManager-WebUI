4544eca68c9c44dc8bf58126550b56c09c1dc2b7
James Mortemore <jamesmortemore@gmail.com>
2021-09-13 00:18:12 +0100

    fix: encrypt test

--- cli/test/encrypt.test.js
+++ cli/test/encrypt.test.js
@@ -4,11 +4,13 @@ const crypto = require('../../server/data/crypto')
 
 describe('encrypt', () => {
   test('should encrypt a value', done => {
-    nixt()
-      .env('ENCRYPTION_KEY', (await crypto.createKey()).toString('hex'))
-      .run('./bin/run encrypt')
-      .on(/Value to encrypt/).respond('test\n')
-      .stdout(/Encrypted value/)
-      .end(done)
+    crypto.createKey().then((key) => {
+      nixt()
+        .env('ENCRYPTION_KEY', key.toString('hex'))
+        .run('./bin/run encrypt')
+        .on(/Value to encrypt/).respond('test\n')
+        .stdout(/Encrypted value/)
+        .end(done)
+    })
   })
 })
