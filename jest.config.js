903d81a8dc2fc31672f94a0f8dc26dcf4c219463
James Mortemore <jamesmortemore@gmail.com>
2022-02-28 20:47:13 +0000

    fix: try again

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   collectCoverage: true,
   testMatch: ['/**/test/*.test.js'],
-  resetModules: true,
-  globals: {
-    'ts-jest': {
-      isolatedModules: true
-    }
-  }
+  resetModules: true
 }
