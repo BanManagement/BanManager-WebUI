4adeb3848afb6ef8a1886fc655dbe595aa981a4b
James Mortemore <jamesmortemore@gmail.com>
2022-03-01 14:53:20 +0000

    fix: ip address conversion

--- server/data/ip.js
+++ server/data/ip.js
@@ -1,84 +1,11 @@
-// Based on https://github.com/kvz/locutus/blob/master/test/languages/php/network/test-inet_ntop.js
-function inetTop (a) {
-  let i = 0
-  let m = ''
-  const c = []
+const { encode, decode } = require('@leichtgewicht/ip-codec')
 
-  a += ''
-  if (a.length === 4) {
-    // IPv4
-    return [
-      a.charCodeAt(0),
-      a.charCodeAt(1),
-      a.charCodeAt(2),
-      a.charCodeAt(3)
-    ].join('.')
-  } else if (a.length === 16) {
-    // IPv6
-    for (i = 0; i < 16; i++) {
-      c.push(((a.charCodeAt(i++) << 8) + a.charCodeAt(i)).toString(16))
-    }
-    return c.join(':')
-      .replace(/((^|:)0(?=:|$))+:?/g, function (t) {
-        m = (t.length > m.length) ? t : m
-        return t
-      })
-      .replace(m || ' ', '::')
-  } else {
-    // Invalid length
-    return null
-  }
+function inetTop (a) {
+  return decode(a)
 }
 
 function inetPton (a) {
-  let m
-  let x
-  let i
-  let j
-  const f = String.fromCharCode
-
-  // IPv4
-  m = a.match(/^(?:\d{1,3}(?:\.|$)){4}/)
-  if (m) {
-    m = m[0].split('.')
-    m = f(m[0]) + f(m[1]) + f(m[2]) + f(m[3])
-    // Return if 4 bytes, otherwise false.
-    return m.length === 4 ? m : false
-  }
-  const r = /^((?:[\da-f]{1,4}(?::|)){0,8})(::)?((?:[\da-f]{1,4}(?::|)){0,8})$/
-
-  // IPv6
-  m = a.match(r)
-  if (m) {
-    // Translate each hexadecimal value.
-    for (j = 1; j < 4; j++) {
-      // Indice 2 is :: and if no length, continue.
-      if (j === 2 || m[j].length === 0) {
-        continue
-      }
-      m[j] = m[j].split(':')
-      for (i = 0; i < m[j].length; i++) {
-        m[j][i] = parseInt(m[j][i], 16)
-        // Would be NaN if it was blank, return false.
-        if (isNaN(m[j][i])) {
-          // Invalid IP.
-          return false
-        }
-        m[j][i] = f(m[j][i] >> 8) + f(m[j][i] & 0xFF)
-      }
-      m[j] = m[j].join('')
-    }
-    x = m[1].length + m[3].length
-    if (x === 16) {
-      return m[1] + m[3]
-    } else if (x < 16 && m[2].length > 0) {
-      return m[1] + (new Array(16 - x + 1))
-        .join('\x00') + m[3]
-    }
-  }
-
-  // Invalid IP
-  return null
+  return encode(a, Buffer.alloc)
 }
 
 module.exports = { inetTop, inetPton }
--- server/test/ip.test.js
+++ server/test/ip.test.js
@@ -3,20 +3,18 @@ const { inetTop, inetPton } = require('../data/ip')
 
 describe('ip', () => {
   describe('#inetTop', () => {
-    test('converts binary to string', () => {
-      assert.strictEqual(inetTop('\x7F\x00\x00\x01'), '127.0.0.1')
+    test('converts buffer to string', () => {
+      assert.strictEqual(inetTop(Buffer.from([0x7F, 0x00, 0x00, 0x01])), '127.0.0.1')
       assert.strictEqual(inetTop('\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'), '::')
-
-      assert.strictEqual(inetTop('asdasd'), null)
+      assert.strictEqual(inetTop(Buffer.from([0x4f, 0xf1, 0x8d, 0x31])), '79.241.141.49')
     })
   })
 
   describe('#inetPton', () => {
-    test('converts string to binary', () => {
-      assert.strictEqual(inetPton('127.0.0.1'), '\x7F\x00\x00\x01')
-      assert.strictEqual(inetPton('::'), '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
-
-      assert.strictEqual(inetPton('asdasd'), null)
+    test('converts string to buffer', () => {
+      assert(inetPton('127.0.0.1').equals(Buffer.from([0x7F, 0x00, 0x00, 0x01])))
+      assert(inetPton('::').equals(Buffer.from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])))
+      assert(inetPton('79.241.141.49').equals(Buffer.from([0x4f, 0xf1, 0x8d, 0x31])))
     })
   })
 })
