0b806f3b1253a7a94cafb54de8cffcc95eddae73
James Mortemore <jamesmortemore@gmail.com>
2022-04-17 17:05:57 +0100

    fix: avoid noop migrations

--- server/data/migrations/20180122081331-acls.js
+++ server/data/migrations/20180122081331-acls.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises
+const fs = require('fs/promises')
 const path = require('path')
 
 exports.up = async function (db) {
--- server/data/migrations/lib/acl.js
+++ server/data/migrations/lib/acl.js
@@ -1,8 +1,6 @@
-const noop = () => {}
-
 module.exports = (db) => {
   async function getResourceId (resourceName) {
-    const [results] = await db.runSql('SELECT resource_id FROM bm_web_resources WHERE name = ?', [resourceName], noop)
+    const [results] = await db.runSql('SELECT resource_id FROM bm_web_resources WHERE name = ?', [resourceName])
 
     return results.resource_id
   }
@@ -10,30 +8,30 @@ module.exports = (db) => {
   return {
     async addPermission (resourceName, ...permissionNames) {
       const resourceId = await getResourceId(resourceName)
-      const [result] = await db.runSql('SELECT COUNT(resource_id) AS i FROM bm_web_resource_permissions WHERE resource_id = ?', [resourceId], noop)
+      const [result] = await db.runSql('SELECT COUNT(resource_id) AS i FROM bm_web_resource_permissions WHERE resource_id = ?', [resourceId])
       let i = result.i
 
       for (const name of permissionNames) {
         const value = Math.pow(2, i++)
 
-        await db.insert('bm_web_resource_permissions', ['resource_id', 'name', 'value'], [resourceId, name, value], noop)
+        await db.insert('bm_web_resource_permissions', ['resource_id', 'name', 'value'], [resourceId, name, value])
       }
     },
     async attachPermission (resourceName, roleId, ...permissionNames) {
       const resourceId = await getResourceId(resourceName)
-      const permissions = await db.runSql('SELECT * FROM bm_web_resource_permissions WHERE resource_id = ? AND name IN(?)', [resourceId, permissionNames], noop)
+      const permissions = await db.runSql('SELECT * FROM bm_web_resource_permissions WHERE resource_id = ? AND name IN(?)', [resourceId, permissionNames])
 
       return Promise.all(permissions.map((permission) => {
-        return db.runSql(`UPDATE bm_web_role_resources SET value = value + ${permission.value} WHERE role_id = ? AND resource_id = ?`, [roleId, resourceId], noop)
+        return db.runSql(`UPDATE bm_web_role_resources SET value = value + ${permission.value} WHERE role_id = ? AND resource_id = ?`, [roleId, resourceId])
       }))
     },
     async addResource (name) {
-      const { insertId: resourceId } = await db.insert('bm_web_resources', ['name'], [name], noop)
+      const { insertId: resourceId } = await db.insert('bm_web_resources', ['name'], [name])
 
-      return db.runSql('INSERT INTO bm_web_role_resources (`role_id`, `resource_id`, `value`) SELECT `role_id`, ?, 0 FROM bm_web_roles', [resourceId], noop)
+      return db.runSql('INSERT INTO bm_web_role_resources (`role_id`, `resource_id`, `value`) SELECT `role_id`, ?, 0 FROM bm_web_roles', [resourceId])
     },
     async removeResource (name) {
-      return db.runSql('DELETE FROM bm_web_resources WHERE name = ?', [name], noop)
+      return db.runSql('DELETE FROM bm_web_resources WHERE name = ?', [name])
     }
   }
 }
