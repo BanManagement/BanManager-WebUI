60a6801200008267c8d557e1e21812a11791d2d5
James Mortemore <jamesmortemore@gmail.com>
2022-07-29 00:11:14 +0100

    feat: group notifications together

--- server/graphql/resolvers/queries/list-notifications.js
+++ server/graphql/resolvers/queries/list-notifications.js
@@ -6,24 +6,41 @@ module.exports = async function listNotifications (obj, { limit, offset }, { ses
   if (limit > 50) throw new ExposedError('Limit too large')
 
   const data = {}
-  const filter = {
-    player_id: session.playerId
-  }
+  const filter = state.dbPool('bm_web_notifications AS n1')
+    .select(state.dbPool.raw('MIN(n1.id)'))
+    .where('n1.server_id', '=', state.dbPool.raw('n.server_id'))
+    .andWhere(builder => {
+      builder
+        .where('n1.report_id', '=', state.dbPool.raw('n.report_id'))
+        .orWhereNull('n1.report_id')
+    })
+    .andWhere('n1.player_id', '=', state.dbPool.raw('n.player_id'))
+    .andWhere(builder => {
+      builder
+        .where('n1.appeal_id', '=', state.dbPool.raw('n.appeal_id'))
+        .orWhereNull('n1.appeal_id')
+    })
   const parsedResolveInfoFragment = parseResolveInfo(info)
   const { fields } = simplifyParsedResolveInfoFragmentWithType(parsedResolveInfoFragment, info.returnType)
 
   if (fields.total) {
-    const { total } = await state.dbPool('bm_web_notifications')
+    const { total } = await state.dbPool('bm_web_notifications AS n')
       .select(state.dbPool.raw('COUNT(*) as total'))
-      .where(filter)
+      .where('n.id', '=', filter)
+      .andWhere({
+        player_id: session.playerId
+      })
       .first()
 
     data.total = total
   }
 
   if (fields.records) {
-    const results = await state.dbPool('bm_web_notifications')
-      .where(filter)
+    const results = await state.dbPool('bm_web_notifications AS n')
+      .where('n.id', '=', filter)
+      .andWhere({
+        player_id: session.playerId
+      })
       .orderBy('updated', 'DESC')
       .limit(limit)
       .offset(offset)
