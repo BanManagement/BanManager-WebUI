9a68551e02d1e24a8b51f952c505ed38467369c4
James Mortemore <jamesmortemore@gmail.com>
2022-07-29 01:27:10 +0100

    feat: show new features and fixes in admin dashboard

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,19 +1,53 @@
+import { AiOutlinePlus } from 'react-icons/ai'
+import { TiTick } from 'react-icons/ti'
+import { upperFirst } from 'lodash-es'
 import Message from '../../components/Message'
 import AdminLayout from '../../components/AdminLayout'
 import { currentVersion } from '../../utils'
 
 export async function getStaticProps () {
+  const version = currentVersion()
+
   let latestVersion = 'unknown'
-  const response = await fetch('https://api.github.com/repos/BanManagement/BanManager-WebUI/commits/master')
+  let response
+
+  if (/\b([a-f0-9]{40})\b/.test(version)) {
+    response = await fetch(`https://api.github.com/repos/BanManagement/BanManager-WebUI/compare/${version}...master`)
+  } else {
+    response = await fetch('https://api.github.com/repos/BanManagement/BanManager-WebUI/commits/master')
+  }
+
   const data = await response.json()
 
-  latestVersion = data.sha
+  if (data?.commits?.length) {
+    latestVersion = data.commits[data.commits.length - 1].sha
+  }
 
-  return { props: { latestVersion }, revalidate: 3600 }
+  const newFeatures = data.commits.filter(({ commit }) => commit.message.startsWith('feat:'))
+  const fixes = data.commits.filter(({ commit }) => commit.message.startsWith('fix:'))
+
+  return { props: { latestVersion, version, newFeatures, fixes }, revalidate: 3600 }
 }
 
-function Page ({ latestVersion }) {
-  const version = currentVersion()
+const formatCommitMessage = (message) => upperFirst(
+  message
+    .replace('feat:', '')
+    .replace('fix:', '')
+    .replace(/\(#\d+\)/, '')
+    .trim()
+)
+
+function Page ({ latestVersion, version, newFeatures, fixes }) {
+  const features = newFeatures?.map(item => (
+    <Message.Item key={item.sha}>
+      <AiOutlinePlus className='text-xl inline' /> {formatCommitMessage(item.commit.message)}
+    </Message.Item>
+  ))
+  const bugFixes = fixes?.map(item => (
+    <Message.Item key={item.sha}>
+      <TiTick className='text-xl inline' /> {formatCommitMessage(item.commit.message)}
+    </Message.Item>
+  ))
 
   return (
     <AdminLayout title='Admin'>
@@ -28,7 +62,17 @@ function Page ({ latestVersion }) {
         <Message info>
           <Message.Header>Update Available</Message.Header>
           <Message.List>
-            <Message.Item>Current version: {version}<br />Latest: {latestVersion}</Message.Item>
+            <Message.Item>Current version: {version.slice(0, 7)}<br />Latest: {latestVersion.slice(0, 7)}</Message.Item>
+            {!!newFeatures?.length &&
+              <>
+                <Message.Item className='font-bold text-sm'>New Features</Message.Item>
+                {features}
+              </>}
+            {!!fixes?.length &&
+              <>
+                <Message.Item className='font-bold text-sm'>Fixes</Message.Item>
+                {bugFixes}
+              </>}
           </Message.List>
         </Message>}
     </AdminLayout>
