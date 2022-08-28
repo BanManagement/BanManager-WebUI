7860b89b7acce22d9e3d3e1d22b88467e6bceb3e
James Mortemore <jamesmortemore@gmail.com>
2022-08-28 14:25:41 +0100

    fix: typo

--- server/app.js
+++ server/app.js
@@ -80,7 +80,7 @@ module.exports = async function ({ dbPool, logger, serversPool, disableUI = fals
 
   await apolloServer.start()
 
-  router.use(apolloServer.getMiddleware({ path: (process.env.BASTE_PATH || '') + '/graphql' }))
+  router.use(apolloServer.getMiddleware({ path: (process.env.BASE_PATH || '') + '/graphql' }))
 
   if (handle) {
     router.all('(.*)', async ctx => {
