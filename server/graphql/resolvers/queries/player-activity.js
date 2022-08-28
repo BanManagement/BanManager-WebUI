5c9b8c57740576bac04ec579d2701830c01bb8ed
James Mortemore <jamesmortemore@gmail.com>
2022-08-28 08:18:49 +0100

    fix: activity overwritten punishments ordering

--- server/graphql/resolvers/queries/player-activity.js
+++ server/graphql/resolvers/queries/player-activity.js
@@ -11,6 +11,10 @@ module.exports = async function playerActivity (obj, { serverId, actor, limit, c
     if (createdStart) query.where('created', '>=', createdStart)
     if (createdEnd) query.where('created', '<=', createdEnd)
 
+    query
+      .orderBy('id', 'DESC')
+      .limit(limit)
+
     return query
   }
   const pastFilter = (query) => {
@@ -18,93 +22,97 @@ module.exports = async function playerActivity (obj, { serverId, actor, limit, c
     if (createdStart) query.where('pastCreated', '>=', createdStart)
     if (createdEnd) query.where('pastCreated', '<=', createdEnd)
 
+    query
+      .orderBy('id', 'DESC')
+      .limit(limit)
+
     return query
   }
 
   const { config, pool } = serversPool.get(serverId)
   const banQuery = () => {
     return filter(pool
-      .select(pool.raw('\'BAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'BAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.playerBans))
   }
   const pastBanQuery = () => {
     return pastFilter(pool
-      .select(pool.raw('\'BAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
+      .select('id', pool.raw('\'BAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
       .from(config.tables.playerBanRecords))
   }
   const unbanQuery = () => {
     return filter(pool
-      .select(pool.raw('\'UNBAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
+      .select('id', pool.raw('\'UNBAN\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
       .from(config.tables.playerBanRecords))
   }
   const muteQuery = () => {
     return filter(pool
-      .select(pool.raw('\'MUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'MUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.playerMutes))
   }
   const pastMuteQuery = () => {
     return pastFilter(pool
-      .select(pool.raw('\'MUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
+      .select('id', pool.raw('\'MUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
       .from(config.tables.playerMuteRecords))
   }
   const unmuteQuery = () => {
     return filter(pool
-      .select(pool.raw('\'UNMUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
+      .select('id', pool.raw('\'UNMUTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
       .from(config.tables.playerMuteRecords))
   }
   const ipMuteQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'IPMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.ipMutes))
   }
   const pastIpMuteQuery = () => {
     return pastFilter(pool
-      .select(pool.raw('\'IPMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
+      .select('id', pool.raw('\'IPMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
       .from(config.tables.ipMuteRecords))
   }
   const ipUnmuteQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPUNMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
+      .select('id', pool.raw('\'IPUNMUTE\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
       .from(config.tables.ipMuteRecords))
   }
   const ipBanQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'IPBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.ipBans))
   }
   const pastIpBanQuery = () => {
     return pastFilter(pool
-      .select(pool.raw('\'IPBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
+      .select('id', pool.raw('\'IPBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('\'\' AS player2'), 'reason', 'expired')
       .from(config.tables.ipBanRecords))
   }
   const ipUnbanQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPUNBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
+      .select('id', pool.raw('\'IPUNBAN\' AS type'), pool.raw('ip AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('createdReason AS reason'), 'expired')
       .from(config.tables.ipBanRecords))
   }
   const ipRangeBanQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPRANGEBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('toIp AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'IPRANGEBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('toIp AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.ipRangeBans))
   }
   const pastIpRangeBanQuery = () => {
     return pastFilter(pool
-      .select(pool.raw('\'IPRANGEBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('toIp AS player2'), 'reason', 'expired')
+      .select('id', pool.raw('\'IPRANGEBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), pool.raw('pastCreated AS created'), pool.raw('toIp AS player2'), 'reason', 'expired')
       .from(config.tables.ipRangeBanRecords))
   }
   const ipRangeUnbanQuery = () => {
     return filter(pool
-      .select(pool.raw('\'IPRANGEUNBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('toIp AS player2'), pool.raw('createdReason AS reason'), 'expired')
+      .select('id', pool.raw('\'IPRANGEUNBAN\' AS type'), pool.raw('fromIp AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('toIp AS player2'), pool.raw('createdReason AS reason'), 'expired')
       .from(config.tables.ipRangeBanRecords))
   }
   const noteQuery = () => {
     return filter(pool
-      .select(pool.raw('\'NOTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('message AS reason'), pool.raw('0 AS expired'))
+      .select('id', pool.raw('\'NOTE\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), pool.raw('message AS reason'), pool.raw('0 AS expired'))
       .from(config.tables.playerNotes))
   }
   const warningQuery = () => {
     return filter(pool
-      .select(pool.raw('\'WARNING\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
+      .select('id', pool.raw('\'WARNING\' AS type'), pool.raw('player_id AS player'), pool.raw('actor_id AS actor'), 'created', pool.raw('\'\' AS player2'), 'reason', pool.raw('expires AS expired'))
       .from(config.tables.playerWarnings))
   }
 
@@ -140,12 +148,13 @@ module.exports = async function playerActivity (obj, { serverId, actor, limit, c
   }).flat()
 
   const data = await pool
-    .select('type', 'player', 'actor', 'created', 'player2', 'reason', 'expired')
+    .select('id', 'type', 'player', 'actor', 'created', 'player2', 'reason', 'expired')
     .from(function () {
-      this.unionAll(queries)
+      this.unionAll(queries, true)
         .as('subquery')
     })
     .orderBy('created', 'DESC')
+    .orderBy('id', 'DESC')
     .limit(limit)
 
   const records = data.map(row => {
